c3bebed247b1df427d0de774e009f89b21f091fb
VG20-s <pkll31311@gmail.com>
2024-07-31 03:37:03 +0900

    [#15]api 연결밑 배포

---
+++ src/api/interceptor.js
@@ -0,0 +1,26 @@
+import Axios from "axios";
+import Cookies from "js-cookie";
+
+export const axios = Axios.create({
+  baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL,
+});
+
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get("AccessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers['Content-Type'] = 'application/json'
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export const http = {
+  get: (url) => axios.get(url).then((res) => res.data),
+  post: (url, data = "") => axios.post(url, data).then((res) => res.data),
+  put: (url, data = "") => axios.put(url, data).then((res) => res.data),
+  patch: (url, data = "") => axios.patch(url, data).then((res) => res.data),
+  delete: (url) => axios.delete(url).then((res) => res.data),
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/config/configure.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      suspense: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
